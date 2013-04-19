c2c8103e5dafd4978f02de45e18652aa2ab0feb4
Dmitrij <dmitrijbelikov@gmail.com>
2013-04-19 15:46:14 +0200

    Initial Commit

---
+++ TheSameWave/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TheSameWave
+//
+//  Created by Дмитрий on 19.04.13.
+//  Copyright (c) 2013 SameWave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TheSameWave/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TheSameWave
+//
+//  Created by Дмитрий on 19.04.13.
+//  Copyright (c) 2013 SameWave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
